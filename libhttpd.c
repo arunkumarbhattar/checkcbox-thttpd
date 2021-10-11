a8c7e654511e46696233f7a52fe279321fa07143
John Kastner <john@correctcomputation.com>
2021-10-11 15:01:11 -0400

    Checked httpd_initialize

--- libhttpd.c
+++ libhttpd.c
@@ -197,7 +197,7 @@ char *ol_strcpy(char *dst : itype(_Array_ptr<char>), char *src : itype(_Nt_array
 static int sub_process = 0;
 
 
-static void
+_Checked static void
 check_options( void )
     {
 #if defined(TILDE_MAP_1) && defined(TILDE_MAP_2)
@@ -207,69 +207,64 @@ check_options( void )
     }
 
 
-static void
-free_httpd_server( httpd_server* hs )
+_Checked static void
+free_httpd_server(httpd_server *hs : itype(_Ptr<httpd_server>))
     {
-    if ( hs->binding_hostname != (char*) 0 )
-	free( hs->binding_hostname );
-    if ( hs->cwd != (char*) 0 )
-	free( hs->cwd );
-    if ( hs->cgi_pattern != (char*) 0 )
-	free( hs->cgi_pattern );
-    if ( hs->charset != (char*) 0 )
-	free( hs->charset );
-    if ( hs->p3p != (char*) 0 )
-	free( hs->p3p );
-    if ( hs->url_pattern != (char*) 0 )
-	free( hs->url_pattern );
-    if ( hs->local_pattern != (char*) 0 )
-	free( hs->local_pattern );
-    free( hs );
+    if ( hs->binding_hostname !=  0 )
+	free<char>( hs->binding_hostname );
+    if ( hs->cwd !=  0 )
+	free<char>( hs->cwd );
+    if ( hs->cgi_pattern !=  0 )
+	free<char>( hs->cgi_pattern );
+    if ( hs->charset !=  0 )
+	free<char>( hs->charset );
+    if ( hs->p3p !=  0 )
+	free<char>( hs->p3p );
+    if ( hs->url_pattern !=  0 )
+	free<char>( hs->url_pattern );
+    if ( hs->local_pattern !=  0 )
+	free<char>( hs->local_pattern );
+    free<httpd_server>( hs );
     }
 
 
-httpd_server*
-httpd_initialize(
-    char* hostname, httpd_sockaddr* sa4P, httpd_sockaddr* sa6P,
-    unsigned short port, char* cgi_pattern, int cgi_limit, char* charset,
-    char* p3p, int max_age, char* cwd, int no_log, FILE* logfp,
-    int no_symlink_check, int vhost, int global_passwd, char* url_pattern,
-    char* local_pattern, int no_empty_referrers )
+_Checked httpd_server *httpd_initialize(char *hostname : itype(_Nt_array_ptr<char>), httpd_sockaddr *sa4P : itype(_Ptr<httpd_sockaddr>), httpd_sockaddr *sa6P : itype(_Ptr<httpd_sockaddr>), unsigned short port, char *_cgi_pattern : itype(_Nt_array_ptr<char>), int cgi_limit, char *charset : itype(_Nt_array_ptr<char>), char *p3p : itype(_Nt_array_ptr<char>), int max_age, char *cwd : itype(_Nt_array_ptr<char>) count(4096), int no_log, FILE *logfp : itype(_Ptr<FILE>), int no_symlink_check, int vhost, int global_passwd, char *url_pattern : itype(_Nt_array_ptr<char>), char *local_pattern : itype(_Nt_array_ptr<char>), int no_empty_referrers) : itype(_Ptr<httpd_server>)
     {
-    httpd_server* hs;
-    static char ghnbuf[256];
-    char* cp;
+    _Nt_array_ptr<char> cgi_pattern = _cgi_pattern;
+    _Ptr<httpd_server> hs = ((void *)0);
+    static char ghnbuf _Nt_checked[256];
+    _Nt_array_ptr<char> cp = ((void *)0);
 
     check_options();
 
     hs = NEW( httpd_server, 1 );
-    if ( hs == (httpd_server*) 0 )
+    if ( hs ==  0 )
 	{
 	syslog( LOG_CRIT, "out of memory allocating an httpd_server" );
-	return (httpd_server*) 0;
+	return (_Ptr<httpd_server>) 0;
 	}
 
-    if ( hostname != (char*) 0 )
+    if ( hostname !=  0 )
 	{
-	hs->binding_hostname = strdup( hostname );
-	if ( hs->binding_hostname == (char*) 0 )
+	hs->binding_hostname = ((_Nt_array_ptr<char> )strdup( hostname ));
+	if ( hs->binding_hostname ==  0 )
 	    {
 	    syslog( LOG_CRIT, "out of memory copying hostname" );
-	    return (httpd_server*) 0;
+	    return (_Ptr<httpd_server>) 0;
 	    }
 	hs->server_hostname = hs->binding_hostname;
 	}
     else
 	{
-	hs->binding_hostname = (char*) 0;
-	hs->server_hostname = (char*) 0;
-	if ( gethostname( ghnbuf, sizeof(ghnbuf) ) < 0 )
+	hs->binding_hostname = (_Nt_array_ptr<char>) 0;
+	hs->server_hostname = (_Nt_array_ptr<char>) 0;
+	if ( gethostname( ghnbuf, sizeof(ghnbuf) - 1 ) < 0 )
 	    ghnbuf[0] = '\0';
 #ifdef SERVER_NAME_LIST
 	if ( ghnbuf[0] != '\0' )
 	    hs->server_hostname = hostname_map( ghnbuf );
 #endif /* SERVER_NAME_LIST */
-	if ( hs->server_hostname == (char*) 0 )
+	if ( hs->server_hostname ==  0 )
 	    {
 #ifdef SERVER_NAME
 	    hs->server_hostname = SERVER_NAME;
@@ -281,58 +276,59 @@ httpd_initialize(
 	}
 
     hs->port = port;
-    if ( cgi_pattern == (char*) 0 )
-	hs->cgi_pattern = (char*) 0;
+    if ( cgi_pattern ==  0 )
+	hs->cgi_pattern = (_Nt_array_ptr<char>) 0;
     else
 	{
 	/* Nuke any leading slashes. */
-	if ( cgi_pattern[0] == '/' )
-	    ++cgi_pattern;
-	hs->cgi_pattern = strdup( cgi_pattern );
-	if ( hs->cgi_pattern == (char*) 0 )
+	if ( cgi_pattern[0] == '/' ) {
+            ++cgi_pattern;
+        }
+	hs->cgi_pattern = ((_Nt_array_ptr<char> )strdup( cgi_pattern ));
+	if ( hs->cgi_pattern ==  0 )
 	    {
 	    syslog( LOG_CRIT, "out of memory copying cgi_pattern" );
-	    return (httpd_server*) 0;
+	    return (_Ptr<httpd_server>) 0;
 	    }
 	/* Nuke any leading slashes in the cgi pattern. */
-	while ( ( cp = strstr( hs->cgi_pattern, "|/" ) ) != (char*) 0 )
+	while ( ( cp = ((_Nt_array_ptr<char> )strstr( hs->cgi_pattern, "|/" )) ) !=  0 )
 	    (void) ol_strcpy( cp + 1, cp + 2 );
 	}
     hs->cgi_limit = cgi_limit;
     hs->cgi_count = 0;
-    hs->charset = strdup( charset );
-    hs->p3p = strdup( p3p );
+    hs->charset = ((_Nt_array_ptr<char> )strdup( charset ));
+    hs->p3p = ((_Nt_array_ptr<char> )strdup( p3p ));
     hs->max_age = max_age;
-    hs->cwd = strdup( cwd );
-    if ( hs->cwd == (char*) 0 )
+    hs->cwd = ((_Nt_array_ptr<char> )strdup( cwd ));
+    if ( hs->cwd ==  0 )
 	{
 	syslog( LOG_CRIT, "out of memory copying cwd" );
-	return (httpd_server*) 0;
+	return (_Ptr<httpd_server>) 0;
 	}
-    if ( url_pattern == (char*) 0 )
-	hs->url_pattern = (char*) 0;
+    if ( url_pattern ==  0 )
+	hs->url_pattern = (_Nt_array_ptr<char>) 0;
     else
 	{
-	hs->url_pattern = strdup( url_pattern );
-	if ( hs->url_pattern == (char*) 0 )
+	hs->url_pattern = ((_Nt_array_ptr<char> )strdup( url_pattern ));
+	if ( hs->url_pattern ==  0 )
 	    {
 	    syslog( LOG_CRIT, "out of memory copying url_pattern" );
-	    return (httpd_server*) 0;
+	    return (_Ptr<httpd_server>) 0;
 	    }
 	}
-    if ( local_pattern == (char*) 0 )
-	hs->local_pattern = (char*) 0;
+    if ( local_pattern ==  0 )
+	hs->local_pattern = (_Nt_array_ptr<char>) 0;
     else
 	{
-	hs->local_pattern = strdup( local_pattern );
-	if ( hs->local_pattern == (char*) 0 )
+	hs->local_pattern = ((_Nt_array_ptr<char> )strdup( local_pattern ));
+	if ( hs->local_pattern ==  0 )
 	    {
 	    syslog( LOG_CRIT, "out of memory copying local_pattern" );
-	    return (httpd_server*) 0;
+	    return (_Ptr<httpd_server>) 0;
 	    }
 	}
     hs->no_log = no_log;
-    hs->logfp = (FILE*) 0;
+    hs->logfp = (_Ptr<FILE>) 0;
     httpd_set_logfp( hs, logfp );
     hs->no_symlink_check = no_symlink_check;
     hs->vhost = vhost;
@@ -343,11 +339,11 @@ httpd_initialize(
     ** like some other systems, it has magical v6 sockets that also listen for
     ** v4, but in Linux if you bind a v4 socket first then the v6 bind fails.
     */
-    if ( sa6P == (httpd_sockaddr*) 0 )
+    if ( sa6P ==  0 )
 	hs->listen6_fd = -1;
     else
 	hs->listen6_fd = initialize_listen_socket( sa6P );
-    if ( sa4P == (httpd_sockaddr*) 0 )
+    if ( sa4P ==  0 )
 	hs->listen4_fd = -1;
     else
 	hs->listen4_fd = initialize_listen_socket( sa4P );
@@ -355,20 +351,20 @@ httpd_initialize(
     if ( hs->listen4_fd == -1 && hs->listen6_fd == -1 )
 	{
 	free_httpd_server( hs );
-	return (httpd_server*) 0;
+	return (_Ptr<httpd_server>) 0;
 	}
 
     init_mime();
 
     /* Done initializing. */
-    if ( hs->binding_hostname == (char*) 0 )
+    if ( hs->binding_hostname ==  0 )
 	syslog(
 	    LOG_NOTICE, "%.80s starting on port %d", SERVER_SOFTWARE,
 	    (int) hs->port );
     else
 	syslog(
 	    LOG_NOTICE, "%.80s starting on %.80s, port %d", SERVER_SOFTWARE,
-	    httpd_ntoa( hs->listen4_fd != -1 ? sa4P : sa6P ),
+	    ((_Nt_array_ptr<char> )httpd_ntoa( hs->listen4_fd != -1 ? sa4P : sa6P )),
 	    (int) hs->port );
     return hs;
     }
